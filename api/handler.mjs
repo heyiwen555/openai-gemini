7603148f41b83745ef4c48059a6969cc90ad0f51
coolyw <13268998+heyiwen555@users.noreply.github.com>
2026-09-08 23:10:24 +0800

    Update handler.mjs

--- api/handler.mjs
+++ api/handler.mjs
@@ -4,24 +4,5 @@ export default worker.fetch;
 
 export const config = {
   runtime: "edge",
-  regions: [
-    "arn1",
-    "bom1",
-    "cdg1",
-    "cle1",
-    "cpt1",
-    "dub1",
-    "hkg1",
-    "fra1",
-    "gru1",
-    "hnd1",
-    "iad1",
-    "icn1",
-    "kix1",
-    "lhr1",
-    "pdx1",
-    "sfo1",
-    "sin1",
-    "syd1",
-  ],
+  regions: ["iad1"],
 };
